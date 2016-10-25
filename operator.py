f3cdcc0fb708f796bda6a487b09103c4524baada
wjcgithub <wjc163@sina.cn>
2016-10-25 12:04:41 +0800

    添加算数操作符

---
+++ operator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# +
+print(1+2)
+# -
+print(12-1)
+# *
+print(12*11)
+# /
+print(12/3)
+# //
+print(12//5)
+# %
+print(12%5)
+# not
+print(not True)
+# and
+print(1 and -1)
+print((True and True))
+# or
+print(1 or -1)
+print(1 or 1)
