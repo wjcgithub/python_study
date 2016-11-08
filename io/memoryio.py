859cd6c1cf16e634a8c8ee40aa8ca9476e6783dc
wjcgithub <wjc163@sina.cn>
2016-11-08 19:56:13 +0800

    添加io函数操作

---
+++ io/memoryio.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+__author__ = 'Wangjichao'
+
+from io import BytesIo,StringIo
+
+# print(dir(io))
+#
+f = StringIO()
+# f.write('hello')
+# ['BlockingIOError', 'BufferedIOBase', 'BufferedRWPair', 'BufferedRandom', 'BufferedReader', 'BufferedWriter', 'BytesIO', 'DEFAULT_BUFFER_SIZE', 'FileIO', 'IOBase', 'IncrementalNewlineDecoder', 'OpenWrapper', 'RawIOBase', 'SEEK_CUR', 'SEEK_END', 'SEEK_SET', 'StringIO', 'TextIOBase', 'TextIOWrapper', 'UnsupportedOperation', '__all__', '__author__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', '_io', 'abc', 'open']
